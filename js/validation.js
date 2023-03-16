3d37e6438b80af0ef4f4342dc96bbd28a5d87a20
AxeKam333 <olek.kaminski5@gmail.com>
2023-03-16 18:56:39 +0100

    style setup

--- js/validation.js
+++ js/validation.js
@@ -9,3 +9,30 @@
     Powodzenia!
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+  }
+
+function isAlphabet(str) {
+  var letters = /^[A-Za-z]+$/;
+  return letters.test(str);
+}
+
+var firstName = document.getElementById("first-name").value;
+const form = document.querySelector("form");
+const input = form.querySelector("input");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if (isEmpty(firstName) || firstName.length <= 2) {
+        alert("Podaj imię!");
+        return false;
+        } else if (!isAlphabet(firstName)) {
+        alert("Imię może zawierać tylko litery!");
+        return false;
+        }
+    form.submit();
+    
+})
